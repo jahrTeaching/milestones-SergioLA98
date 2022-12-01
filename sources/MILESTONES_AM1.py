cc006e75a0e7727d9fab9a04f06b3b92729f25df
SergioLA98 <sergio.lopez.acedo@alumnos.upm.es>
2022-12-01 12:42:42 +0100

    Update

--- sources/MILESTONES_AM1.py
+++ sources/MILESTONES_AM1.py
@@ -3,10 +3,10 @@ from numpy import array
 
 #import MILESTONE_01
 #import MILESTONE_02
-import MILESTONE_03
+#import MILESTONE_03
 #MILESTONE_01.prueba()
 # N = 
 
 #MILESTONE_02.SimulationMIL2(  tf = 20, N = 201, U0 = array( [ 1., 0., 0., 1. ] ) )
 
-MILESTONE_03.ErrorNumerico3( tf = 5, N = 101, U0 = array( [ 1., 0., 0., 1. ] ) )
+#MILESTONE_03.ErrorNumerico3( tf = 5, N = 101, U0 = array( [ 1., 0., 0., 1. ] ) )
