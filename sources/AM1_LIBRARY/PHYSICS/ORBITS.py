1f2d8e2f0527a7a91ae2aefb6e72164ec1bdaf18
SergioLA98 <sergio.lopez.acedo@alumnos.upm.es>
2022-12-10 02:52:00 +0100

    MIL6 upd2

--- sources/AM1_LIBRARY/PHYSICS/ORBITS.py
+++ sources/AM1_LIBRARY/PHYSICS/ORBITS.py
@@ -5,8 +5,8 @@ Created on Wed Oct  5 18:29:45 2022
 @author: serg_
 """
 
-from numpy import array 
-
+from numpy import array, sqrt, zeros
+from scipy.optimize import newton, fsolve
 
 def Kepler(U, t): 
 
@@ -14,3 +14,35 @@ def Kepler(U, t):
     d = ( x**2  +y**2 )**1.5
 
     return  array( [ dxdt, dydt, -x/d, -y/d ] ) 
+
+def CR3BP(U, t, mu): #Circular Restricted 3 Body Problem
+    
+    x = U[0]; y = U[1]
+    vx = U[2]; vy = U[3]
+    
+    r1 = sqrt( (x+mu)**2 + y**2 )
+    r2 = sqrt( (x-1+mu)**2 + y**2 )
+    
+    dxdt = vx
+    dydt = vy
+    
+    dvxdt = 2*vy+x-( (1-mu)*(x+mu) ) / (r1**3) - mu*(x+mu-1)/(r2**3)
+    dvydt = -2*vx + y -( (1-mu) / (r1**3) + mu/(r2**3) )*y
+    
+    return array([ dxdt, dydt, dvxdt, dvydt])
+
+def LG_Points( U0 ,N ,mu ):
+    
+    LGP = zeros([5,2])
+
+    def F(Y):
+        X = zeros(4)
+        X[0:2] = Y
+        X[2:4] = 0
+        FX = CR3BP(X, 0 , mu)
+        return FX[2:4]
+   
+    for i in range(N):
+        LGP[i,:] = fsolve(F, U0[i,0:2])
+        
+    return LGP
--- sources/MILESTONE_06.py
+++ sources/MILESTONE_06.py
@@ -1,26 +1,28 @@
 from numpy import zeros, linspace, array
 import matplotlib.pyplot as plt
 
-from AM1_LIBRARY.PHYSICS.ORBITS import Kepler
+from AM1_LIBRARY.PHYSICS.ORBITS import CR3BP, LG_Points
 from AM1_LIBRARY.ODES.TEMP_SCHEMES import Embedded_RK
 from AM1_LIBRARY.ODES.EDO import CauchyProblem
 
 
+#mu = 3.0039e-7 #Tierra-Sol
+mu = 1.2151e-2 #Tierra-Luna
 
-U0 = array( [ 1., 0., 0., 1. ] )
+U0_LG = zeros([5,4]) #5 Puntos Lagrange, 2 coord. De normal era vector 1,4 lo que mando a Cauchy
+U0_LG[0,:] = [0.8, 0.6, 0, 0]
+U0_LG[1,:] = [0.8, -0.6, 0, 0]
+U0_LG[2,:] = [-0.1, 0, 0, 0]
+U0_LG[3,:] = [0.1, 0, 0, 0]
+U0_LG[4,:] = [1.01, 0, 0, 0]
 t = linspace(0, 20, 2001) #da N puntos el linspace
-U = CauchyProblem( Kepler, t, U0, Embedded_RK)
 
-plt.plot(U[:,0] , U[:,1])
-plt.show()
+LP = LG_Points(U0_LG, 5, mu)
+print(LP)
+def F(U,t):
+    return CR3BP(U,t,mu)  #wrapped ya que Schemes usa F(U,t) y aqui doy mu tb
 
-'''
+#U = CauchyProblem( F, t, U0, Embedded_RK)
 
-U0 = zeros(6,5) #6coordinates and 5 points
-U0[:,0] = [ 0.8, 0.6, 0., 0., 0., 0.  ]   # Lagrange points
-U0[:,1] = [ 0.8, -0.6, 0., 0., 0., 0.  ]
-U0[:,2] = [ -0.1, 0.0, 0., 0., 0., 0.  ]
-U0[:,3] = [ 0.1, 0.0, 0., 0., 0., 0.  ]
-U0[:,4] = [ 1.1, 0.0, 0., 0., 0., 0.  ]
-
-'''
+#plt.plot(U[:,0] , U[:,1])
+#plt.show()
